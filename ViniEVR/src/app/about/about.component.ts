5d5c427ef9e827aed4d5caa4e35f9ffded4463c8
Vinicius <vinimice42@gmail.com>
2023-09-03 03:49:44 -0300

    [FEATURE] - Adicionando conteúdo na aba sobre mim

--- ViniEVR/src/app/about/about.component.ts
+++ ViniEVR/src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2, ElementRef, OnInit, ViewChild  } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -7,10 +7,29 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./about.component.sass']
 })
 export class AboutComponent implements OnInit{
-
-  constructor(private titleService: Title){}
+  @ViewChild('bottom') bottom: ElementRef | undefined;
+  yearsOld(): number{
+    const birthday = new Date('2000-09-04');
+      const currentDate = new Date();
+      const differenceInMilliseconds = currentDate.getTime() - birthday.getTime();
+      const yearsOld = (Math.floor(differenceInMilliseconds / (1000 * 3600 * 24 * 365.25))) + 1;
+      return yearsOld
+  }
+  scrollToBottom() {
+    if (this.bottom) {
+      this.bottom.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' 
+    });
+  }
+  constructor(private titleService: Title, private renderer: Renderer2, private el: ElementRef){}
 
   ngOnInit(): void {
+    this.renderer.setStyle(document.body, 'overflow', 'hidden');
     this.titleService.setTitle('About - Vinícius')
   }
 }
